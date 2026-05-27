6b19e41e08cdaa368574e9d32ba5106de7acb7a6
TamaraLatino <tamaralatino06@gmail.com>
2026-05-26 20:06:38 -0600

    Proyecto Spring Boot base creado

---
+++ backend/src/test/java/com/styleshop/StyleshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.styleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
