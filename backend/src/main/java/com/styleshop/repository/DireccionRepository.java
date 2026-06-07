be4b86ee5c68acdfafd2aaa1ce0bf1a192ee92ec
AbigailAle95 <Abigailalech@gmail.com>
2026-06-07 00:25:23 -0600

    se agrego y termino DireccionRepository

--- backend/src/main/java/com/styleshop/repository/DireccionRepository.java
+++ backend/src/main/java/com/styleshop/repository/DireccionRepository.java
@@ -4,6 +4,9 @@ import com.styleshop.model.Direccion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DireccionRepository extends JpaRepository<Direccion, Long> {
+    List<Direccion> findByUsuario_IdUsuario(Long idUsuario);
 }
